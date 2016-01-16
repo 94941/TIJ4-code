85f6af8174ded2f0e2f788100e1c4303a9584bdb
lileitong <lileitong8023@163.com>
2016-01-16 23:09:11 +0800

    1.初始化项目 2.添加HelloDate类

---
+++ src/com/lott/HelloDate.java
@@ -0,0 +1,13 @@
+package com.lott;
+
+import java.util.Date;
+
+/**
+ * Created by Lott on 2016/1/16.
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello,It's: ");
+        System.out.println(new Date());
+    }
+}
