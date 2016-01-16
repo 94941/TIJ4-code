cac08fc2c17b1ec0f478ac8e8353ffcad8598779
lileitong <lileitong8023@163.com>
2016-01-16 23:29:47 +0800

    添加ShowProperties类

---
+++ src/com/lott/ShowProperties.java
@@ -0,0 +1,12 @@
+package com.lott;
+
+/**
+ * Created by Administrator on 2016/1/16.
+ */
+public class ShowProperties {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
